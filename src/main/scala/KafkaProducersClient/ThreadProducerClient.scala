a0115d0bc40fbca151c30f70615c352a247a9dea
rockdatio <info@rockdatio.com>
2022-05-08 02:33:18 -0500

    fix

--- src/main/scala/KafkaProducersClient/ThreadProducerClient.scala
+++ src/main/scala/KafkaProducersClient/ThreadProducerClient.scala
@@ -24,11 +24,11 @@ class ThreadProducerClient(topic: String,
           println(limit)
           Thread.sleep(sleep.toInt)
           println(message)
-//          Unirest
-//            .post("https://api.powerbi.com/beta/5d93ebcc-f769-4380-8b7e-289fc972da1b/datasets/7265d43e-1d8c-490f-a372-af79a67a0d7e/rows?key=uMMWfIke7bDqFmTqkdkh7qhcCiwybQ1yqlRQkUYidRRGYd2RAjz4CWUHGb%2BOy21QQVYGrA5xog28OV%2BnovWdEg%3D%3D")
-//            .header("Content-Type", "application/json")
-//            .body(message)
-//            .asJsonAsync()
+          //          Unirest
+          //            .post("https://api.powerbi.com/beta/5d93ebcc-f769-4380-8b7e-289fc972da1b/datasets/7265d43e-1d8c-490f-a372-af79a67a0d7e/rows?key=uMMWfIke7bDqFmTqkdkh7qhcCiwybQ1yqlRQkUYidRRGYd2RAjz4CWUHGb%2BOy21QQVYGrA5xog28OV%2BnovWdEg%3D%3D")
+          //            .header("Content-Type", "application/json")
+          //            .body(message)
+          //            .asJsonAsync()
           val record: ProducerRecord[String, String] = new ProducerRecord[String, String](topic, message)
           producer.send(record)
         }
@@ -38,4 +38,5 @@ class ThreadProducerClient(topic: String,
     //    producer.close()
   }
 }
+
 //                      .post("https://api.powerbi.com/beta/c4a66c34-2bb7-451f-8be1-b2c26a430158/datasets/d0539339-c3db-4130-bd2d-70280bca4fa6/rows?key=Vf5M26vH55Fo6HXoSbNIz4Fl9HYTuDnOEmfSgRnPYnOWJNuqKUMBHgVH0hE9sn5HPRsUvVyspAkhJCvoMmbYpg%3D%3D")
